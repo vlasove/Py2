521e094ec3b9848b46317a92966d9c996c00c384
vlasove <evgeny_vlasov@yahoo.com>
2020-12-06 16:59:56 +0300

    Added Lec7-8

--- Lec8/main.py
+++ Lec8/main.py
@@ -1,20 +1,30 @@
 """
 Переопределение (shadowing - затенение)
 """
+class Point:
+    def __init__(self, x, y):
+        self.x_val = x 
+        self.y_val = y 
+    def __str__(self):
+        return f"Point[X:{self.x_val}, Y:{self.y_val}]" 
+    
+    def __eq__(self, other): 
+        return (self.x_val, self.y_val) == (other.x_val, other.y_val) 
+
+    def __gt__(self, other): # Сравнение строго `>`. Аналог __lt__
+        """
+        greater than
+        """
+        return (self.x_val, self.y_val) > (other.x_val, other.y_val)
+
+    def __ge__(self, other): # Сравнение на `>=`. Важно, что `>=` это не `>` + `==`
+        return (self.x_val, self.y_val) >= (other.x_val, other.y_val)
+
+    def __del__(self): # Этот метод вызывается в тот момент, когда происходит передача вашего объекта GC
+        print("ЭТОТ ОБЪЕКТ ПЕРЕДАН ВО ВЛАЖНЫЕ ЛАДОШКИ СБОРЩИКА МУСОРА")
+        print(self)
 
 
-class Calculator:
-    def __init__(self, x:int, y:int):
-        self.x_value = x 
-        self.y_value = y 
-
-
-    def calc(self):
-        return self.x_value + self.y_value
-
-    def calc(self):
-        return self.x_value ** 2 + self.y_value ** 2
-
-
-c = Calculator(2,10)
-print(c.calc())
+p = Point(10, 20)
+1/0
+p2 = Point(30, 40)
